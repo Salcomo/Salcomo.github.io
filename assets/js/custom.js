febfe150f46c52d6f2652bcb637a6f6745f2cb3c
Michael Maggiacomo <mmaggiacomo@rogers.com>
2024-04-28 23:53:27 -0400

    Made prettier and fixed header

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,62 +1,72 @@
-$(document).ready(function(){
-	"use strict";
+$(document).ready(function () {
+  "use strict";
 
-/*=========== TABLE OF CONTENTS ===========
+  /*=========== TABLE OF CONTENTS ===========
 1. Scroll To Top 
 2. Smooth Scroll spy
 3. welcome animation support
 ======================================*/
 
-    // 1. Scroll To Top 
-		$(window).on('scroll',function () {
-			if ($(this).scrollTop() > 600) {
-				$('.return-to-top').fadeIn();
-			} else {
-				$('.return-to-top').fadeOut();
-			}
-		});
-		$('.return-to-top').on('click',function(){
-				$('html, body').animate({
-				scrollTop: 0
-			}, 1500);
-			return false;
-		});
-	
-	
-	
-	// 2. Smooth Scroll spy
-		
-		$('.header-area').sticky({
-           topSpacing:0
-        });
-		
-		//=============
+  // 1. Scroll To Top
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 600) {
+      $(".return-to-top").fadeIn();
+    } else {
+      $(".return-to-top").fadeOut();
+    }
+  });
+  $(".return-to-top").on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1500
+    );
+    return false;
+  });
 
-		$('li.smooth-menu a').bind("click", function(event) {
-			event.preventDefault();
-			var anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $(anchor.attr('href')).offset().top - 0
-			}, 1200,'easeInOutExpo');
-		});
-		
-		$('body').scrollspy({
-			target:'.navbar-collapse',
-			offset:0
-		});
+  // 2. Smooth Scroll spy
 
+  $(".header-area").sticky({
+    topSpacing: 0,
+  });
 
-    // 5. welcome animation support
+  //=============
 
-        $(window).load(function(){
-        	$(".header-text h2,.header-text p").removeClass("animated fadeInUp").css({'opacity':'0'});
-            $(".header-text a").removeClass("animated fadeInDown").css({'opacity':'0'});
-        });
+  $("li.smooth-menu a").bind("click", function (event) {
+    event.preventDefault();
+    var anchor = $(this);
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $(anchor.attr("href")).offset().top - 0,
+        },
+        1200,
+        "easeInOutExpo"
+      );
+  });
 
-        $(window).load(function(){
-        	$(".header-text h2,.header-text p").addClass("animated fadeInUp").css({'opacity':'0'});
-            $(".header-text a").addClass("animated fadeInDown").css({'opacity':'0'});
-        });
+  $("body").scrollspy({
+    target: ".navbar-collapse",
+    offset: 0,
+  });
 
-});	
-	
+  // 5. welcome animation support
+
+  $(window).load(function () {
+    $(".header-text h2,.header-text p")
+      .removeClass("animated fadeInUp")
+      .css({ opacity: "0" });
+    $(".header-text a")
+      .removeClass("animated fadeInDown")
+      .css({ opacity: "0" });
+  });
+
+  $(window).load(function () {
+    $(".header-text h2,.header-text p")
+      .addClass("animated fadeInUp")
+      .css({ opacity: "0" });
+    $(".header-text a").addClass("animated fadeInDown").css({ opacity: "0" });
+  });
+});
